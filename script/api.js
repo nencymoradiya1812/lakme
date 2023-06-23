4ec717de2ba852e4b11527a9fcd6439074f42028
nencymoradiya1812 <nencymoradiya18@gmail.com>
2023-06-23 17:36:51 +0530

    first page completed

---
+++ script/api.js
@@ -0,0 +1,177 @@
+let api = [
+  {
+    "id": 1,
+    "comman": "BEST SELLER",
+    "title": "LAKMÉ 9 TO 5 PRIMER + MATTE LIP COLOR",
+    "price": "₹ 446",
+    "old price": "₹ 550",
+    "offer": "19% OFF",
+    "icon": "fa-solid fa-star",
+    "rent": "4.9|",
+    "image":
+      "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+    "rating": "68 Verified Ratings",
+    "shades": "35 shades",
+    "m-image":
+      "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24846_H-8901030794292_450x.jpg?v=1674041566",
+  },
+  {
+    "id": 2,
+    "comman": "BEST SELLER",
+    "title": "LAKMÉ ABSOLUTE MATTE ULTIMATE LIP COLOR WI...",
+    "price": "₹ 825",
+    "old price": "₹ 550",
+    "offer": "19% OFF",
+    "icon": "fa-solid fa-star",
+    "rent": "4.9|",
+    "image":
+      "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+    "rating": "20 Verified Ratings",
+    "shades": "12 shades",
+    "m-image":
+      "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24345_H-8901030724268_540x.jpg?v=1674040406",
+  },
+  {
+                "id": 3,
+                "comman": "BEST SELLER",
+                "title": "LAKMÉ ABSOLUTE 3D LIPSTICK",
+                "price": "₹ 800",
+                "old price": "₹ 299",
+                "offer": "5% OFF",
+                "icon": "fa-solid fa-star",
+                "rent": "4.8|",
+                "image":
+                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                "rating": "24 Verified Ratings",
+                "shades": "20 shades",
+                "m-image":
+                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24704H8901030776533_540x.png?v=1653496333",
+              },
+              {
+                "id": 4,
+                "comman": "BEST SELLER",
+                "title": "LAKMÉ ABSOLUTE 3D LIPSTICK",
+                "price": "₹ 446",
+                "old price": "₹ 800",
+                "offer": "19% OFF",
+                "icon": "fa-solid fa-star",
+                "rent": "5|",
+                "image":
+                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                "rating": "16 Verified Ratings",
+                "shades": "12 shades",
+                "m-image":
+                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/img1_540x.png?v=1652943920",
+              },
+              {
+                "id": 5,
+                "comman": "BEST SELLER",
+                "title": "LAKMÉ ABSOLUTE MATTE REVOLUTION LIP COLOR",
+                "price": "₹ 800",
+                "old price": "₹ 550",
+                "offer": "19% OFF",
+                "icon": "fa-solid fa-star",
+                "rent": "4.8|",
+                "image":
+                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                "rating": "12 Verified Ratings",
+                "shades": "20 shades",
+                "m-image":
+                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/Envious-Red_6f459ac4-e29a-42ee-a07d-2882b01d7bb8_540x.jpg?v=1653496726"},
+                  {
+                                "id": 6,
+                                "comman": "BEST SELLER",
+                                "title": "LAKMÉ ABSOLUTE PRECISION LIP PAINT",
+                                "price": "₹ 725",
+                                "old price": "₹ 550",
+                                "offer": "19% OFF",
+                                "icon": "fa-solid fa-star",
+                                "rent": "5|",
+                                "image":
+                                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                                "rating": "11 Verified Ratings",
+                                "shades": "10 shades",
+                                "m-image":
+                                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24846_H-8901030794292_450x.jpg?v=1674041566",
+                              },
+                              {
+                                "id": 7,
+                                "comman": "BEST SELLER",
+                                "title": "LAKMÉ ABSOLUTE 3D LIPSTICK",
+                                "price": "₹ 825",
+                                "old price": "₹ 550",
+                                "offer": "19% OFF",
+                                "icon": "fa-solid fa-star",
+                                "rent": "4.5|",
+                                "image":
+                                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                                "rating": "8 Verified Ratings",
+                                "shades": "10 shades",
+                                "m-image":
+                                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24990H28901030817496_540x.png?v=1653545838",
+                              },
+                              {
+                                "id": 8,
+                                "comman": "BEST SELLER",
+                                "title": "LAKMÉ ABSOLUTE 3D LIP DEFINER",
+                                "price": "₹ 638",
+                                "old price": "₹ 750",
+                                "offer": "15% OFF",
+                                "icon": "fa-solid fa-star",
+                                "rent": "4.5|",
+                                "image":
+                                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                                "rating": "4Verified Ratings",
+                                "shades": "6 shades",
+                                "m-image":
+                                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24835_H1-8901030792083_540x.jpg?v=1611903594",
+                              },
+                              {
+                                "id": 9,
+                                "comman": "BEST SELLER",
+                                "title": "BOLD FOREVER MATTE LIPS SET OF 4",
+                                "price": "₹ 1049",
+                                "old price": "₹ 1396",
+                                "offer": "25% OFF",
+                                "icon": "fa-solid fa-star",
+                                "rent": "5|",
+                                "image":
+                                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                                "rating": "10 Verified Ratings",
+                                "shades": "35 shades",
+                                "m-image":
+                                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24846_H-8901030794292_450x.jpg?v=1674041566",
+                              },
+                              {
+                                "id": 10,
+                                "comman": "BEST SELLER",
+                                "title": "ALWAYS ON MATTE LIPSTICK SET",
+                                "price": "₹ 699",
+                                "old price": "₹ 897",
+                                "offer": "22% OFF",
+                                "icon": "fa-solid fa-star",
+                                "rent": "4.6|",
+                                "image":
+                                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                                "rating": "13 Verified Ratings",
+                                "shades": "35 shades",
+                                "m-image":
+                                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/AlwaysOnMatteLipstickSet_540x.jpg?v=1681120336",
+                              },
+                              {
+                                "id": 11,
+                                "comman": "BEST SELLER",
+                                "title": "LAKMÉ ABSOLUTE PRECISION LIP PAINT VALUE SET",
+                                "price": "₹ 999",
+                                "old price": "₹ 1300",
+                                "offer": "23% OFF",
+                                "icon": "fa-solid fa-star",
+                                "rent": "5|",
+                                "image":
+                                  "https://cdn.shopify.com/s/files/1/0560/7017/6931/files/50x50.png?v=1657620495",
+                                "rating": "4 Verified Ratings",
+                                "shades": "35 shades",
+                                "m-image":
+                                  "https://cdn.shopify.com/s/files/1/0014/3514/0183/products/24846_H-8901030794292_450x.jpg?v=1674041566",
+                              },
+];
